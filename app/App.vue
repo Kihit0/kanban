442e32b4016fb80da67fad4fbe8a739449ec5c41
Kihito <benner.andrey.loon@gmail.com>
2024-05-06 15:52:04 +0300

    Adding css in app component

--- app/App.vue
+++ app/App.vue
@@ -2,7 +2,19 @@
 </script>
 
 <template>
+  <div class="app w-screen p-8">
+    <div class="app-wrapper rounded-8 w-full"></div>
+  </div>
 </template>
 
 <style scoped>
+.app {
+  background-color: var(--bg-color-main);
+  height: 100vh;
+  overflow: hidden;
+}
+
+.app-wrapper{
+  height: 100%;
+}
 </style>
